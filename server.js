d667aa0299219fff8e390d191ebb5e6092471e24
Rowan Romanauskas <rowan@romanauskas.com>
2024-03-03 02:45:40 -0600

    msg

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static("express"));
 
 app.use(express.static('public'));
 app.get('', function(req,res){
-    res.sendFile(path.join(__dirname+'/public/html/applyform.html'));
+    res.sendFile(path.join(__dirname+'index.html'));
   });
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -40,19 +40,55 @@ app.post('/submit-form', (req, res) => {
     }
   });
 
-  /*app.post('/edit-form', (req,res) => {
+  app.post('/submit-form-hr', (req, res) => {
+    try {
+      const data = fs.readFileSync('applicants.json', 'utf8');
+      const jsonData = JSON.parse(data);
+      jsonData.applicants.push({
+        name: req.body.stdName,
+        email: req.body.stdEmail,
+        date: req.body.stdDate,
+        location: req.body.question,
+        phone: req.body.stdPhone,
+        school: req.body.stdScool,
+        role: req.body.stdRole,
+        LinkedIn: req.body.stdLinkedIn,
+        resume: req.body.stdResume,
+        progress: "N/A",
+        gauge: "N/A",
+        feedback: "N/A"
+      });
+      fs.writeFileSync('applicants.json', JSON.stringify(jsonData, null, 2));
+    } catch (error) {
+      console.error('Error processing form submission:', error);
+    }
+  });
+
+  app.post('/edit-form', (req,res) => {
     try{
       const data = fs.readFileSync('applicants.json', 'utf8');
       const jsonData = JSON.parse(data);
-      const applicantEmail = req.body.searchVal
-      const applicant = jsonData.applicants.find(applicant => applicant.email == applicantEmail)
+      const applicantEmail = req.body.searchVal;
+      const applicant = jsonData.applicants.find(applicant => applicant.email == applicantEmail);
       if(applicant) {
-        
-      }
-    }
-  }catch(error){
+        applicant.name = req.body.stdName;
+        applicant.email = req.body.stdEmail;
+        applicant.date = req.body.stdDate;
+        applicant.location = req.body.question;
+        applicant.phone = req.body.stdPhone;
+        applicant.school = req.body.stdScool;
+        applicant.role = req.body.stdRole;
+        applicant.LinkedIn = req.body.stdLinkedIn;
+        applicant.progress = req.body.stdProgress;
+        applicant.gauge = req.body.stdGauge;
+        applicant.feedback = req.body.stdFeedback;
 
-  });*/
+        fs.writeFileSync('applicants.json', JSON.stringify(jsonData,null, 2));
+      }
+    } catch(error){
+      console.error("Error editing applicant: ", error);
+    }
+  });
   
 const server = http.createServer(app);
 const port = 3000;
